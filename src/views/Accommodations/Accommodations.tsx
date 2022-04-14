b1f932c90a87d538ca3a1728df8bf7ffdd637749
Ian Sibner <sibnerian@gmail.com>
2022-04-14 18:55:49 -0400

    Add a link to actually book accommodations

--- src/views/Accommodations/Accommodations.tsx
+++ src/views/Accommodations/Accommodations.tsx
@@ -23,12 +23,15 @@ const Accommodations = () => (
                   <p>4200 City Ave, Philadelphia, PA 19131</p>
                 </div>
               </EqualHeightElement>
-              <div className="blurb">
-                <p>Rooms are available at a reduced rate of $149 / night (plus tax).</p>
-                <p>
-                  Call <a href="tel:800-445-8667">800-445-8667</a> to book. The block is reserved
-                  under Dornblaser/Sibner Wedding.
-                </p>
+              <EqualHeightElement name="blurb">
+                <div className="blurb">
+                  Rooms are available at a reduced rate of $149 / night (plus tax).
+                </div>
+              </EqualHeightElement>
+              <div>
+                <a href="www.my-event.hilton.com/phlphhf-dsw-263b5af4-538b-4bd8-a65f-b5ea8a0dcfbf/">
+                  <div className="cta">Book now</div>
+                </a>
               </div>
             </div>
           </Col>
@@ -40,9 +43,11 @@ const Accommodations = () => (
               <EqualHeightElement name="address">
                 <div className="address">Various locations</div>
               </EqualHeightElement>
-              <div className="blurb">
-                There are a variety of options available at Airbnb to fit any taste and budget.
-              </div>
+              <EqualHeightElement name="blurb">
+                <div className="blurb">
+                  There are a variety of options available at Airbnb to fit any taste and budget.
+                </div>
+              </EqualHeightElement>
               <div>
                 <a href="https://www.airbnb.com/s/Chestnut-Hill--Philadelphia--Pennsylvania--United-States/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_dates%5B%5D=april&flexible_trip_dates%5B%5D=may&flexible_trip_lengths%5B%5D=weekend_trip&date_picker_type=calendar&query=Chestnut%20Hill%2C%20Philadelphia%2C%20Pennsylvania%2C%20United%20States&place_id=ChIJySfomnG5xokRhhIKOhXKRlE&checkin=2022-09-23&checkout=2022-09-25&source=structured_search_input_header&search_type=autocomplete_click">
                   <div className="cta">View stays</div>
